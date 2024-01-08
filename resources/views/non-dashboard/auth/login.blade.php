91b82dcfaf33f5099cf41e800bee7436bbca17ab
HizaCrenata <princespiniq@gmail.com>
2024-01-08 08:50:52 +0700

    Halaman Login Fix

--- resources/views/non-dashboard/auth/login.blade.php
+++ resources/views/non-dashboard/auth/login.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master-without-nav')
 
-@include('layouts.nav')
+<!--@include('layouts.nav')-->
 @section('content')
 
 <div class="auth-page-wrapper pt-5">
@@ -8,12 +8,12 @@
     <div class="auth-one-bg-position auth-one-bg"  id="auth-particles">
         <div class="bg-overlay"></div>
 
-        <!-- 
+        
         <div class="shape">
             <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1440 120">
                 <path d="M 0,36 C 144,53.6 432,123.2 720,124 C 1008,124.8 1296,56.8 1440,40L1440 140L0 140z"></path>
             </svg>
-        </div> -->
+        </div> 
     </div>
 
     <!-- auth page content -->
@@ -34,7 +34,7 @@
 
             <div class="row justify-content-center">
                 <div class="col-md-8 col-lg-6 col-xl-5">
-                    <div class="card mt-4">
+                    <div class="card mt-4" style="z-index: 10">
 
                         <div class="card-body p-4">
                             <div class="text-center mt-2">
